f376bb685c52f98b3c0b88e9575c968f1d74ad8d
Ali <150735175+Ali2038@users.noreply.github.com>
2025-06-18 20:18:55 -0300

    Add files via upload

---
+++ function/funcao9.py
@@ -0,0 +1,8 @@
+def soma (a, b, c):
+    return a+ b+ c
+
+if __name__ == '__main__':
+    valor_um = int(input("Número: "))
+    valor_dois = int(input("Número: "))
+    valor_tres = int(input("Número: "))
+    print("Resultado: ", soma(valor_um, valor_dois, valor_tres))
